5995b4bf0c5ce6e56baf28dbd51a9b82904815d7
Charvi Khurana <67728901+charvi03@users.noreply.github.com>
2024-05-27 10:22:02 +0530

    Update Courses.js

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -12,13 +12,36 @@ import {
 
 function Courses() {
   const [courses, setCourses] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     axios
       .get("https://freecodecamp-miniclone.onrender.com/getcourses")
-      .then((response) => setCourses(response.data))
-      .catch((err) => console.log(err));
+      .then((response) => {
+        setCourses(response.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
   }, []);
+
+  const spinnerStyle = {
+    border: "16px solid #f3f3f3",
+    borderTop: "16px solid #3498db",
+    borderRadius: "50%",
+    width: "120px",
+    height: "120px",
+    animation: "spin 2s linear infinite",
+  };
+
+  const spinnerContainerStyle = {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100vh", // Full height of the viewport
+  };
 
   return (
     <div className="bg-gray-100 text-black p-8 flex flex-col items-center">
@@ -27,40 +50,46 @@ function Courses() {
         "I have not failed, I've just found 10,000 ways that won't work" -
         Thomas A. Edison
       </p>
-      <div className="w-full md:w-2/3 lg:w-1/2">
-        {courses.map((course, index) => (
-          <div
-            key={course._id}
-            className="border border-black rounded p-4 bg-gray-200 mb-4 flex justify-between items-center"
-          >
-            <div className="flex items-center">
-              {index === 0 && (
-                <FaJs className="text-yellow-500 mr-2 text-2xl" />
-              )}
-              {index === 1 && (
-                <FaPython className="text-blue-600 mr-2 text-2xl" />
-              )}
-              {index === 2 && (
-                <FaReact className="text-blue-400 mr-2 text-2xl" />
-              )}
-              {index === 3 && (
-                <FaAngular className="text-red-600 mr-2 text-2xl" />
-              )}
-              {index === 4 && (
-                <FaDatabase className="text-gray-500 mr-2 text-2xl" />
-              )}
-              {index === 5 && (
-                <FaNodeJs className="text-green-500 mr-2 text-2xl" />
-              )}
-              {index === 6 && (
-                <FaBrain className="text-purple-600 mr-2 text-2xl" />
-              )}
-              <div className="font-bold">{course.title}</div>
+      {loading ? (
+        <div style={spinnerContainerStyle}>
+          <div style={spinnerStyle}></div>
+        </div>
+      ) : (
+        <div className="w-full md:w-2/3 lg:w-1/2">
+          {courses.map((course, index) => (
+            <div
+              key={course._id}
+              className="border border-black rounded p-4 bg-gray-200 mb-4 flex justify-between items-center"
+            >
+              <div className="flex items-center">
+                {index === 0 && (
+                  <FaJs className="text-yellow-500 mr-2 text-2xl" />
+                )}
+                {index === 1 && (
+                  <FaPython className="text-blue-600 mr-2 text-2xl" />
+                )}
+                {index === 2 && (
+                  <FaReact className="text-blue-400 mr-2 text-2xl" />
+                )}
+                {index === 3 && (
+                  <FaAngular className="text-red-600 mr-2 text-2xl" />
+                )}
+                {index === 4 && (
+                  <FaDatabase className="text-gray-500 mr-2 text-2xl" />
+                )}
+                {index === 5 && (
+                  <FaNodeJs className="text-green-500 mr-2 text-2xl" />
+                )}
+                {index === 6 && (
+                  <FaBrain className="text-purple-600 mr-2 text-2xl" />
+                )}
+                <div className="font-bold">{course.title}</div>
+              </div>
+              <div>({course.duration})</div>
             </div>
-            <div>({course.duration})</div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
